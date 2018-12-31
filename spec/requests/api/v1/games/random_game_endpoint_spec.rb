f3bfa45a6064e90da0289b86c8dd7f56b1325e7a
jtrtj <jt.r@live.com>
2018-12-31 14:24:57 -0700

    Write test for random game endpoint

---
+++ spec/requests/api/v1/games/random_game_endpoint_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe '/api/v1' do
+  context 'GET /games/random' do
+    it 'returns a single random question' do
+      questions = create_list(:question, 10)
+
+      get '/api/v1/games/random'
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result[:questions]).to all(have_key(:question))
+      expect(result[:questions]).to all(have_key(:correct_answer))
+      expect(result[:questions][:answers]).to all(be_an(Array))
+      expect(result[:questions]).to all(have_key(:image_url))
+      expect(result[:questions]).to all(have_key(:image_url))
+      expect(result[:questions]).to all(have_key(:image_author))
+      expect(result[:questions]).to all(have_key(:image_author_profile))
+    end
+  end
+end
