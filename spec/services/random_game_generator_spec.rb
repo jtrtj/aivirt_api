9ba880d0d5d9b7f82ff0a1fa5d27d73b6dcb028f
jtrtj <jt.r@live.com>
2018-12-31 15:06:14 -0700

    Add another expectation for random_game_generator spec

--- spec/services/random_game_generator_spec.rb
+++ spec/services/random_game_generator_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 describe RandomGameGenerator do
   context 'instance methods' do
+    before :each do
+      create_list(:question, 30)
+    end
     it '#jsonable_hash - returns a hash containing question attributes' do
       generator = RandomGameGenerator.new
+      game_data = generator.jsonable_hash
 
-      expect(generator.jsonable_hash).to be_a(Hash)
+      expect(game_data).to be_a(Hash)
+      expect(game_data[:questions].count).to eq(10)
     end
     it '#get_ten_questions - returns 10 existing questions from db' do
       generator = RandomGameGenerator.new
